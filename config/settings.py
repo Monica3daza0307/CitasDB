79c95a41259d1c9ca5ad72846836b4486c15b406
Monica Daza <mldazal@academia.usbbog.edu.co>
2025-09-07 22:43:59 -0500

    Se logo borrar la primer cita

--- config/settings.py
+++ config/settings.py
@@ -7,5 +7,21 @@ CITAS = [
         "fecha": "2025-09-06",
         "hora": "10:00",
         "motivo": "Chequeo general"
+    },
+    {
+        "id": 2,
+        "paciente": "Maria Rodriguez",
+        "medico": "Dra. Sandra Milena",
+        "fecha": "2025-09-07",
+        "hora": "11:00",
+        "motivo": "Cita Oncologica"
+    },
+    {
+        "id": 3,
+        "paciente": "Milena Suarez",
+        "medico": "Dr. Juan Alonso",
+        "fecha": "2025-09-08",
+        "hora": "12:00",
+        "motivo": "Cita Ginecología"
     }
 ]
