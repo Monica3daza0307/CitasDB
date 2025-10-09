6bcecbe6a2acd7bb281438bfe8f9a9153ad9b56b
Monica Daza <mldazal@academia.usbbog.edu.co>
2025-10-09 17:05:39 -0500

    apply changes

--- repositories/cita_repository.py
+++ repositories/cita_repository.py
@@ -1,33 +1,27 @@
 # repositories/cita_repository.py
 from config.settings import CITAS
-from models.cita_model import Cita
 
 class CitaRepository:
     def get_all(self):
-        return [Cita(**c) for c in CITAS]
+        return CITAS
 
     def get_by_id(self, cita_id):
-        for c in CITAS:
-            if c["id"] == cita_id:
-                return Cita(**c)
-        return None
+        return next((c for c in CITAS if c["id"] == cita_id), None)
 
     def create(self, data):
-        new_id = max(c["id"] for c in CITAS) + 1 if CITAS else 1
-        data["id"] = new_id
         CITAS.append(data)
-        return Cita(**data)
+        return data
 
     def update(self, cita_id, data):
-        for i, c in enumerate(CITAS):
-            if c["id"] == cita_id:
+        for i, cita in enumerate(CITAS):
+            if cita["id"] == cita_id:
                 CITAS[i].update(data)
-                return Cita(**CITAS[i])
+                return CITAS[i]
         return None
 
     def delete(self, cita_id):
-        for c in CITAS:
-            if c["id"] == cita_id:
-                CITAS.remove(c)
-                return True
+        cita = self.get_by_id(cita_id)
+        if cita:
+            CITAS.remove(cita)
+            return True
         return False
