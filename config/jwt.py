98f5afa821789a71395f1d1737cb53f7a31b87de
Monica Daza <mldazal@academia.usbbog.edu.co>
2025-11-05 22:45:01 -0500

    Se agrega configuración JWT en mi config/jwt.py

---
+++ config/jwt.py
@@ -0,0 +1,10 @@
+# config/jwt.py
+
+import os
+
+# Configuración de JWT
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+JWT_TOKEN_LOCATION = ["headers"]
+JWT_ACCESS_TOKEN_EXPIRES = 3600  # 1 hora
+JWT_HEADER_NAME = "Authorization"
+JWT_HEADER_TYPE = "Bearer"
